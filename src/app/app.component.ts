d84194d003f6df5387056d731b10f96fc094eefd
juan <juanhatumvarela@gmail.com>
2021-06-19 00:23:23 -0500

    ya se realiza la busqueda y se visualizan en pantalla los hoteles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,14 @@ import {HotelsService} from './services/hotels.service';
 })
 export class AppComponent {
   hotels: any;
-  hotelscopy: any;
+  hotelscopy: any[] = [];
+  searchPerformed: any[];
+  flag: boolean;
+  message: string;
+  finded: string;
+  result: any[];
   constructor( private service: HotelsService) {
+    this.flag = false;
     this.service.getHotels()
       .subscribe((hotels: any) => {
         console.log(hotels);
@@ -17,4 +23,30 @@ export class AppComponent {
         this.hotelscopy = this.hotels;
       });
   }
+
+  // tslint:disable-next-line:typedef
+  search() {
+    if (this.finded === undefined) {
+      this.hotelscopy = [];
+      this.flag = true;
+      this.message = 'esta vacia la barra de busqueda';
+    } else {
+      this.hotelscopy = this.hotels;
+      this.searchPerformed = [];
+      this.hotelscopy.forEach( data => {
+        const hotel: string = data.name.toLowerCase();
+        if ( hotel.search(this.finded.toLowerCase()) >= 0) {
+          this.searchPerformed.push(data);
+        }
+        if (this.searchPerformed.length > 0) {
+          this.flag = false;
+        } else {
+          this.flag = true;
+          this.message = 'No hay información referente a ese nombre de Hotel';
+        }
+      });
+      this.hotelscopy = this.searchPerformed;
+    }
+  }
+
 }
